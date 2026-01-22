0e44af921dd76ff2b7cc5927b4256dab108fdd70
Dev <devaram@duck.com>
2026-01-22 15:34:13 +0530

    bug fixed v

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-    fetch('../.version')
+    fetch('https://raw.githubusercontent.com/HindiWeb/math10th/main/.version')
         .then(r => r.text())
         .then(v => {
             v = hwapi().h(v.trim()).a(hwapi('.markdown-preview').get()).get();
