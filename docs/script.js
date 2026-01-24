3b93f1d8be5f8f05f559719c517a21ec9ade1504
Dev <devaram@duck.com>
2026-01-24 15:56:16 +0530

    paper 5

--- docs/script.js
+++ docs/script.js
@@ -1,42 +1,28 @@
+const vURL =
+  "https://raw.githubusercontent.com/HindiWeb/math10th/main/.version";
+let currentVersion;
+
 window.addEventListener("DOMContentLoaded", function () {
-    // fetch("https://raw.githubusercontent.com/HindiWeb/math10th/main/.version")
-    //     .then((r) => r.text())
-    //     .then((v) => {
-    //         v = hwapi().h(v.trim()).a(hwapi(".markdown-preview").get()).get();
-    //         v.id = "version";
-    //     });
-
-    const vURL =
-        "https://raw.githubusercontent.com/HindiWeb/math10th/main/.version";
-    let etag;
-    let currentVersion;
-    async function checkVersion() {
-        const res = await fetch(vURL, {
-            cache: "no-cache",
-            headers: etag ? { "If-None-Match": etag } : {},
-        });
-
-        if (res.status === 304) return;
-
-        if (res.ok) {
-            etag = res.headers.get("etag");
-            const v = (await res.text()).trim();
-            if (currentVersion && v !== currentVersion) location.reload(true); // reload when currentVersion is set and version changed
-            renderVersion(v);
-            currentVersion = v;
+  async function checkVersion() {
+    fetch(vURL, {  cache: "no-store"  })
+      .then((r) => r.text())
+      .then((v) => {
+        if(!currentVersion) renderVersion(v);
+        if (currentVersion && v !== currentVersion) {
+          location.reload(true);
         }
-    }
+        currentVersion = v;
+      });
+  }
 
-    setInterval(checkVersion, 30000);
-});
-function renderVersion(v) {
+  setInterval(checkVersion, 30000);
+  function renderVersion(v) {
     if (document.getElementById("version")) {
-        hwapi("#version").h(v);
+      hwapi("#version").h(v);
     } else {
-        const vEl = hwapi()
-            .h(v)
-            .a(hwapi(".markdown-preview").get())
-            .get();
-        vEl.id = "version";
+      const vEl = hwapi().h(v).a(hwapi(".markdown-preview").get() || hwapi().a().get()).get();
+      vEl.id = "version";
     }
-}
+  }
+  checkVersion();
+});
